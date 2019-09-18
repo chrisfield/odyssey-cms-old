9bc2f625c463d1fd1be9ee04fadbe248d2ce8449
chrisfield <chris@christopherfield.co.uk>
2019-09-18 10:43:20 +0100

    Add widgets to graphql schema

--- packages/data-server/schema/index.js
+++ packages/data-server/schema/index.js
@@ -1,15 +1,58 @@
 const graphql = require('graphql');
 
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+const {
+  GraphQLObjectType,
+  GraphQLID,
+  GraphQLString,
+  GraphQLSchema,
+  GraphQLList
+} = graphql;
 
 const BookType = new GraphQLObjectType({
   name: 'Book',
   fields: () => ({
-    id: {type: GraphQLString},
+    id: {type: GraphQLID},
     name:{type: GraphQLString},
     genre: {type: GraphQLString}
   })
 });
+
+const SlotType = new GraphQLObjectType({
+  name: 'Slot',
+  fields: () => ({
+    widget:{type: GraphQLString}
+  })
+});
+
+const SectionType = new GraphQLObjectType({
+  name: 'Section',
+  fields: () => ({
+    id: {type: GraphQLID},
+    name:{type: GraphQLString},
+    slots:{type: new GraphQLList(SlotType)}
+  })
+});
+
+
+const sections = [
+  {
+    id: "1",
+    name: "slot one",
+    slots: [
+      {widget: 'w1'},
+      {widget: 'w2'}
+    ]
+  },
+  {
+    id: "2",
+    name: "slot2",
+    slots: [
+      {widget: 'ww1'},
+      {widget: 'ww2'}
+    ]
+
+  }
+];
 
 const books = [
   {
@@ -29,10 +72,19 @@ const rootQuery = new GraphQLObjectType({
   fields: {
     book: {
       type: BookType,
-      args: {id: {type: GraphQLString}},
+      args: {id: {type: GraphQLID}},
       resolve(_parent, args) {
         return books.find((book) => (
           book.id === args.id
+        ));
+      }
+    },
+    section: {
+      type: SectionType,
+      args: {id: {type: GraphQLID}},
+      resolve(_parent, args) {
+        return sections.find((section) => (
+          section.id === args.id
         ));
       }
     }
